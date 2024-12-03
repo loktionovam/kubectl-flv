51d8654abe82aec7ba285d63008d2c9d9b056e15
Aleksandr Loktionov <a.loktionov@sdventures.com>
2024-12-03 17:54:36 +0400

    Add configmaps and secrets inspector

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 if [ -d "$HOME/.krew/bin/" ]; then
     INSTALL_DIR="$HOME/.krew/bin/"
-    cp -av kubectl-flv kubectl-flv-preview "$INSTALL_DIR"
+    cp -av kubectl-flv kubectl-flv-preview kubectl-flv-cm-view "$INSTALL_DIR"
 else
     INSTALL_DIR=/usr/local/bin/
-    sudo cp -av kubectl-flv kubectl-flv-preview "$INSTALL_DIR"
+    sudo cp -av kubectl-flv kubectl-flv-preview kubectl-flv-cm-view "$INSTALL_DIR"
 fi
